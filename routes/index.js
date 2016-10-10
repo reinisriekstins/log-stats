a38c8fd6456a64a071bf20522007ee92b637b095
Reinis Riekstins <riekstins.reinis@gmail.com>
2016-10-10 19:02:48 +0300

    Added data validation.

--- routes/index.js
+++ routes/index.js
@@ -3,80 +3,83 @@
 var express = require('express');
 var router = express.Router();
 
-const request = require("request");
-const _       = require("lodash");
+const request = require('request');
+const _       = require('lodash');
+const cheerio = require('cheerio');
+const url     = require('url');
+
+const requestArray = require('../logs');
+
+function tryParseJSON(jsonString) {
+  var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+  try {
+    var o = JSON.parse(jsonString);
+
+    // Handle non-exception-throwing cases:
+    // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+    // but... JSON.parse(null) returns null, and typeof null === "object",
+    // so we must check for that, too. Thankfully, null is falsey, so this suffices:
+    if (o && (typeof o === 'undefined' ? 'undefined' : _typeof(o)) === "object") {
+      return o;
+    }
+  } catch (e) {
+    console.log('ERROR: Couldn\'t parse the following JSON: ' + e);
+    return false;
+  }
+
+  return jsonString;
+}
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-
-  var requestArray = [
-    "http://logs.tf/json/1467290",
-    "http://logs.tf/json/1467270",
-    "http://logs.tf/json/1470116",
-    "http://logs.tf/json/1470090",
-    "http://logs.tf/json/1472397",
-    "http://logs.tf/json/1474249",
-    "http://logs.tf/json/1474288",
-    "http://logs.tf/json/1475194",
-    "http://logs.tf/json/1477260",
-    "http://logs.tf/json/1477309",
-    "http://logs.tf/json/1479187",
-    "http://logs.tf/json/1479222",
-    "http://logs.tf/json/1481895",
-    "http://logs.tf/json/1481929",
-    "http://logs.tf/json/1483724",
-    "http://logs.tf/json/1483759",
-    "http://logs.tf/json/1484569",
-    "http://logs.tf/json/1507118",
-    "http://logs.tf/json/1507152",
-    "http://logs.tf/json/1508969",
-    "http://logs.tf/json/1508994",
-    "http://logs.tf/json/1514335",
-    "http://logs.tf/json/1514379",
-    "http://logs.tf/json/1516345",
-    "http://logs.tf/json/1516372",
-    "http://logs.tf/json/1517294",
-    "http://logs.tf/json/1517330",
-    "http://logs.tf/json/1517362",
-    "http://logs.tf/json/1519074",
-    "http://logs.tf/json/1519102",
-    "http://logs.tf/json/1520298",
-    "http://logs.tf/json/1521573",
-    "http://logs.tf/json/1521619",
-    "http://logs.tf/json/1523536",
-    "http://logs.tf/json/1523567",
-    "http://logs.tf/json/1526363",
-    "http://logs.tf/json/1526422",
-    "http://logs.tf/json/1527040",
-    "http://logs.tf/json/1527069",
-    "http://logs.tf/json/1532687",
-    "http://logs.tf/json/1537423",
-    "http://logs.tf/json/1537459"
-  ];
 
   // extract the JSON strings from all the logs.tf links
   var responseArray = _.map(requestArray, eachRequest => {
 
     var eachResponse = new Promise((resolve, reject) => {
 
-      request(eachRequest, (error, response, body) => { error ? reject(error) : resolve(body) });
+      // check if the link has "http://" in it's beginning, if it doesn't,
+      // add it, because otherwise the url parser can't parse it properly
+      !/^http:\/\//gi.test(eachRequest) ? eachRequest = 'http://' + eachRequest : '';
+      
+      var requestUrl = url.parse(eachRequest);
+      var host = requestUrl.host,
+          path = requestUrl.path;
 
+      if (requestUrl.host     === 'logs.tf' || 'www.logs.tf' &&
+          requestUrl.protocol ===  null     || 'http:'       &&
+          requestUrl.path.match(/^\d{5,}$/g)) {
+
+        request('http://' + host + '/json' + path, (error, response, body) => {
+
+          if (error) {
+            reject(error);
+          }
+          else if ( !tryParseJSON(body) ) {
+            reject(requestUrl.href + ' doesn\'t seem to be a valid log. \n');
+          }
+          else {
+            resolve(body);
+          }
+        });
+      }
+      else reject(requestUrl.host + ' didn\'t validate');
     });
 
     return eachResponse;
-
   });
 
   // when all log JSON strings have been extracted
   Promise
     .all(responseArray)
     .then(logsArray => {
+
       // convert all the JSON strings to javascript objects
-      logsArray = _.map(logsArray, log => JSON.parse(log));
+      logsArray = _.map(logsArray, log => tryParseJSON(log));
 
       var players = {};
 
-      // for each log
       logsArray.forEach(log => {
 
         if (Object.keys(players).length === 0) {
@@ -142,7 +145,7 @@ router.get('/', (req, res, next) => {
               player.cpc           += logPlayer.cpc;
               player.ic            += logPlayer.ic;
               player.time_played   += log.length;
-              
+
               player.kpd  = (player.deaths === 0) ? 0 : (player.kills / player.deaths);
               player.kpad = (player.deaths === 0) ? 0 : ((player.kills + player.assists) / player.deaths);
 
